d5f687c994bcdf5ea154fd3b1a5d50fcc030dd3e
samet uğur aslan <sametugur.aslan@hotmail.com>
2021-06-12 14:13:49 +0300

    Vücut Kitle index hesaplama eklendi

--- src/dilimAlma.java
+++ src/dilimAlma.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class dilimAlma {
+import java.util.Scanner;
+
+public class dilimAlma {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Dairenin Yarı çapını giriniz : ");
+        int r = input.nextInt();
+        System.out.print("Merkez açısının ölçüsünü giriniz : ");
+        int a = input.nextInt();
+
+        double result =  (pi * (r*r) * a) / 360;
+
+        System.out.println("sonuç " + result);
+    }
 }
---
+++ src/vucutKitle.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class vucutKitle {
+    public static void main(String[] args) {
+        double boy, index  ;
+        int kilo ;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Boyunuzu giriniz : ");
+        boy = input.nextDouble();
+        System.out.print("Kilonuzu giriniz : ");
+        kilo = input.nextInt();
+
+        index = kilo/(boy * boy);
+        System.out.print(index);
+
+    }
+}
